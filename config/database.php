a4a1486ff795d816894cf6644a33b0846e2a9c44
ExtraTech Web <extratechweb@gmail.com>
2022-10-11 15:07:50 +0545

    updates

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => 'ams_server',
             'username' => 'root',
-            'password' => '',
+            'password' => 'Annapurna123@',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
