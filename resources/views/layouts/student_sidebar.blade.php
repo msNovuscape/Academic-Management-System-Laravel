0de0984df702c02a1ffaa3a8b03a72d1622ff83d
kctribiking <kctribiking@gmail.com>
2022-10-17 15:28:30 +0545

    [edit] student detail css fixed

--- resources/views/layouts/student_sidebar.blade.php
+++ resources/views/layouts/student_sidebar.blade.php
@@ -1,13 +1,13 @@
 <ul class="nav">
-    <li class="nav-item">
+    <li class="nav-item @if(Request::segment(1) === 'student') active @endif">
         <a class="nav-link" href="{{url('/')}}" aria-expanded="false" aria-controls="ui-basic">
             <div class="sidebar-icon">
                 <img src="{{url('images/batch-icon.png')}}" alt="sidebar-icon"/>
-                <span class="menu-title">Details</span>
+                <span class="menu-title">Student Details</span>
             </div>
         </a>
     </li>
-    <li class="nav-item">
+    <li class="nav-item @if(Request::segment(2) === 'materials') active @endif">
         <a class="nav-link"  href="{{url('student/materials')}}" aria-expanded="false" aria-controls="ui-basic">
             <div class="sidebar-icon">
                 <img src="{{url('images/course-icon.png')}}" alt="sidebar-icon"/>
