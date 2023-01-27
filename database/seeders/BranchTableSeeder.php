d0db9325824d7e2a4994038b66361123952db1cd
ExtraTech Web <extratechweb@gmail.com>
2023-01-27 14:37:25 +0545

    BranchTableSeede Updated

--- database/seeders/BranchTableSeeder.php
+++ database/seeders/BranchTableSeeder.php
@@ -16,10 +16,22 @@ class BranchTableSeeder extends Seeder
     public function run()
     {
         DB::table('branches')->insert([
-            'name' => 'Sydney Branch',
+            ['name' => 'Sydney Branch',
             'address' => 'Sydney,Australia',
             'phone_no' => '9813890246',
-            'status' => 1
+            'status' => 1],
+            ['name' => 'Canberra',
+                'address' => 'Canberra,Australia',
+                'phone_no' => '61261696243',
+                'status' => 1],
+            ['name' => 'Brisbane',
+                'address' => 'Brisbane,Australia',
+                'phone_no' => '61450319193',
+                'status' => 1],
+            ['name' => 'Melbourne',
+                'address' => 'Melbourne,Australia',
+                'phone_no' => '61291717742',
+                'status' => 1],
         ]);
     }
 }
